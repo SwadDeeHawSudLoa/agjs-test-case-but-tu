ba57c3f05c6c89a6214bbcb4ace139c3b1cafc78
NooobtimeX <61542428+Nooobtime@users.noreply.github.com>
2023-04-04 19:50:02 +0700

    new

--- spec.js
+++ spec.js
@@ -7,39 +7,87 @@ describe("Login Form", function () {
   });
   it("should fill in the username and password fields and click the login button", async function () {
     //login
-    var usernameField = element(by.id("i_username"));
-    var passwordField = element(by.id("i_password"));
-    var loginButton = element(by.tagName("button"));
-    await browser.wait(EC.visibilityOf(usernameField), 10000);
-    await browser.wait(EC.visibilityOf(passwordField), 10000);
-    await browser.wait(EC.visibilityOf(loginButton), 10000);
-    await usernameField.sendKeys("consult");
-    await passwordField.sendKeys("consult");
-    await loginButton.click();
+    await browser.wait(EC.visibilityOf(element(by.id("i_username"))), 10000);
+    await element(by.id("i_username")).sendKeys("consult");
+    await browser.wait(EC.visibilityOf(element(by.id("i_password"))), 10000);
+    await element(by.id("i_password")).sendKeys("consult");
+    await browser.wait(EC.visibilityOf(element(by.tagName("button"))), 10000);
+    await element(by.tagName("button")).click();
     //1.1 click Outbound
-    var sidebarMenu = element(by.id("sidebar-menu"));
-    await browser.wait(EC.visibilityOf(sidebarMenu), 10000);
-    await sidebarMenu.click();
+    await browser.wait(EC.visibilityOf(element(by.id("sidebar-menu"))), 10000);
+    await element(by.id("sidebar-menu")).click();
     //1.2 click salesOrder
-    var salesOrderLink = element
+    await browser.wait(
+      EC.elementToBeClickable(
+        element.all(by.cssContainingText("a", "Sales Orders")).first()
+      ),
+      10000
+    );
+    await element
       .all(by.cssContainingText("a", "Sales Orders"))
-      .first();
-    await browser.wait(EC.elementToBeClickable(salesOrderLink), 10000);
-    await salesOrderLink.click();
+      .first()
+      .click();
     //2.Create SO and 3.
-    var createItemButton = element(by.id("i_create_item"));
-    await browser.wait(EC.elementToBeClickable(createItemButton), 10000);
-    await createItemButton.click();
+    await browser.wait(
+      EC.elementToBeClickable(element(by.id("i_create_item"))),
+      10000
+    );
+    await element(by.id("i_create_item")).click();
     //4.a
-    var select = element(by.xpath('//*[@id="tabstrip-2"]/form/div[3]/div[1]/div/span[1]/span[1]'))
-    await browser.wait(EC.visibilityOf(select), 10000);
-    await select.click();
-    var options = element(by.xpath('//*[@id="i_client_listbox"]/li[2]'))
-    await browser.wait(EC.visibilityOf(options), 10000);
-    await options.click();
-    await browser.sleep(10000);
+    await browser.wait(
+      EC.visibilityOf(
+        element(
+          by.xpath(
+            '//*[@id="tabstrip-2"]/form/div[3]/div[1]/div/span[1]/span[1]'
+          )
+        )
+      ),
+      10000
+    );
+    await element(
+      by.xpath('//*[@id="tabstrip-2"]/form/div[3]/div[1]/div/span[1]/span[1]')
+    ).click();
+    await browser.wait(
+      EC.visibilityOf(element(by.xpath('//*[@id="i_client_listbox"]/li[2]'))),
+      10000
+    );
+    await element(by.xpath('//*[@id="i_client_listbox"]/li[2]')).click();
     //4.b
+    await browser.wait(
+      EC.visibilityOf(element(by.id("i_customer_insert"))),
+      10000
+    );
+    await element(by.id("i_customer_insert")).sendKeys("10009670");
+    await browser.wait(
+      EC.visibilityOf(
+        element(by.xpath('//*[@id="i_customer_insert_listbox"]/li'))
+      ),
+      10000
+    );
+    await element(by.xpath('//*[@id="i_customer_insert_listbox"]/li')).click();
     //4.c
+    await browser.wait(
+      EC.visibilityOf(
+        element(
+          by.xpath('//*[@id="tabstrip-2"]/form/div[5]/div[1]/div/span[1]/span')
+        )
+      ),
+      10000
+    );
+    await element(
+      by.xpath('//*[@id="tabstrip-2"]/form/div[5]/div[1]/div/span[1]/span')
+    ).click();
+    await browser.wait(
+      EC.visibilityOf(
+        element(
+          by.xpath('//*[@id="i_shipto_address_insert_listbox"]/li[2]/span')
+        )
+      ),
+      10000
+    );
+    await element(
+      by.xpath('//*[@id="i_shipto_address_insert_listbox"]/li[2]/span')
+    ).click();
     //4.d
     //4.e
     //4.f
@@ -61,14 +109,19 @@ describe("Login Form", function () {
     await browser.wait(EC.elementToBeClickable(updateButton), 10000);
     await updateButton.click();*/
     //9 logout
-    var userProfileDropdown = element(by.css(".user-profile.dropdown-toggle"));
-    await browser.wait(EC.elementToBeClickable(userProfileDropdown), 10000);
-    await userProfileDropdown.click();
+    await browser.sleep(10000);
+    await browser.wait(
+      EC.elementToBeClickable(element(by.css(".user-profile.dropdown-toggle"))),
+      10000
+    );
+    await element(by.css(".user-profile.dropdown-toggle")).click();
     await browser.sleep(1000);
     var dropdownMenu = element(by.css(".dropdown-menu.dropdown-usermenu"));
-    var lastListItem = dropdownMenu.all(by.css("li")).last();
-    await browser.wait(EC.elementToBeClickable(lastListItem), 10000);
-    await lastListItem.click();
+    await browser.wait(
+      EC.elementToBeClickable(dropdownMenu.all(by.css("li")).last()),
+      10000
+    );
+    await dropdownMenu.all(by.css("li")).last().click();
   });
 });
 
